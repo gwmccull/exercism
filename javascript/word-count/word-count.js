e3ddfee8ecc32c67aca410fbb6bd717a1b5a0bc2
Garrett McCullough <garrett.mccullough@clearcapital.com>
2015-05-12 22:07:05 -0700

    javascript/word-count refactor using Object.create()

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,9 +1,9 @@
 var words = function(input) {
-    var subStrings = input.replace(/\n|\t|\s+/g, ' ').split(' ');
-    var total = {};
+    var subStrings = input.replace(/\s+/, ' ').split(' ');
+    var total = Object.create(null);
 
     subStrings.forEach(function(string) {
-        if (typeof total[string] === 'undefined' || typeof total[string] !== 'number') {
+        if (typeof total[string] === 'undefined') {
             total[string] = 0;
         }
 
